d808fa1379c86968bb7701589c1152c4199cf059
Mateusz Lacek <mlacek19@comcast.net>
2023-01-06 10:29:53 -0600

    Add conditionals for who wins based on selected choice. Add condition for invalid entry.

--- script.js
+++ script.js
@@ -16,4 +16,40 @@ function oneRound(computerSelection, playerSelection) {
     computerSelection = getComputerChoice();
     playerSelection = prompt("Rock, Paper, or Scissors?")
     playerSelection = playerSelection.toLowerCase();
+    if (playerSelection !== "rock" && playerSelection !== "scissors" && playerSelection !== "paper") {
+        return "Invalid entry. Rock, Paper, or Scissors are the only valid entries."
+    }
+    if (computerSelection === "rock") {
+        if (playerSelection === "rock") {
+            return "Tie!";
+        }
+        else if (playerSelection === "paper") {
+            return "You win! Paper beats rock";
+        }
+        else if (playerSelection === "scissors") {
+            return "You lose! Rock beats paper";
+        }
+    }
+    if (computerSelection === "paper") {
+        if (playerSelection === "rock") {
+            return "You lose! Paper beats rock";
+        }
+        else if (playerSelection === "paper") {
+            return "Tie!";
+        }
+        else if (playerSelection === "scissors") {
+            return "You win! scissors beats paper";
+        }
+    }
+    if (computerSelection === "scissors") {
+        if (playerSelection === "rock") {
+            return "You win! Rock beats scissors";
+        }
+        else if (playerSelection === "paper") {
+            return "You lose! scissors beats paper";
+        }
+        else if (playerSelection === "scissors") {
+            return "Tie!";
+        }
+    }
 }
