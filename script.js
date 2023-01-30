fc096cd5f450bb6f51a89495e333443030f18559
Mateusz Lacek <mlacek19@comcast.net>
2023-01-30 09:58:56 -0600

    Add div for score. change returns to console.log for temporary visualization.

--- script.js
+++ script.js
@@ -22,51 +22,51 @@ function oneRound(playerSelection) {
         if (playerSelection === "rock") {
             console.log("Tie!");
             tieCount += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
         else if (playerSelection === "paper") {
             console.log("You win! Paper beats rock");
             playerScore += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
         else if (playerSelection === "scissors") {
             console.log("You lose! Rock beats paper");
             compScore += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
     }
     if (computerSelection === "paper") {
         if (playerSelection === "rock") {
             console.log("You lose! Paper beats rock");
             compScore += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
         else if (playerSelection === "paper") {
             console.log("Tie!");
             tieCount += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
         else if (playerSelection === "scissors") {
             console.log("You win! scissors beats paper");
             playerScore += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
     }
     if (computerSelection === "scissors") {
         if (playerSelection === "rock") {
             console.log("You win! Rock beats scissors");
             playerScore += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
         else if (playerSelection === "paper") {
             console.log("You lose! scissors beats paper");
             compScore += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
         else if (playerSelection === "scissors") {
             console.log("Tie!");
             tieCount += 1;
-            return "Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount
+            console.log("Computer: " + compScore + " Player: " + playerScore + " Tie: " + tieCount)
         }
     }
 }
